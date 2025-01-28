c54063e66113431806e372125152b499df645dd0
hasidmansoori <67381761+hasidmansoori@users.noreply.github.com>
2025-01-28 23:55:07 +0530

    28-01-2025
    latest commit

--- src/pages/Winners.js
+++ src/pages/Winners.js
@@ -5,19 +5,19 @@ const winners = [
     {
       id: "1",
       name: "Ahmed Ali",
-      picture: "https://via.placeholder.com/150", // Static image URL
+      picture: "https://www.karachiliteraturefestival.com/wp-content/uploads/2022/02/Ahmed-Ali-Akbar.png", // Static image URL
       testimonial: "I'm so happy to win! This is the best prize ever!",
     },
     {
       id: "2",
       name: "Sara Mohammed",
-      picture: "https://via.placeholder.com/150", // Static image URL
+      picture: "https://www.reedsmith.com/-/media/images/professionals/m/mohammed-sara/highresolutionimage/mohammeds300.jpg?rev=cd7ee3227ff14551a33d7174bf5b618c&hash=54D3EE3485BC31679B61C1D77453BF62", // Static image URL
       testimonial: "Thank you so much for this amazing prize! Totally worth it.",
     },
     {
       id: "3",
       name: "Omar Hassan",
-      picture: "https://via.placeholder.com/150", // Static image URL
+      picture: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2_N3CyjlYhmDYPipwOIzllwBA_qLDqMqGNw&s", // Static image URL
       testimonial: "I can't believe I won! This contest is amazing.",
     },
   ];
@@ -40,7 +40,8 @@ const Winners = ({ language }) => {
       <ul>
         {winners.map((winner) => (
           <li key={winner._id}>
-            <img src={winner.picture} alt={winner.name} />
+            <img src={winner.picture} alt={winner.name} 
+            style={{ width: '200px', height: '200px' }}/>
             <p>{winner.name}</p>
             <p>{winner.testimonial}</p>
           </li>
